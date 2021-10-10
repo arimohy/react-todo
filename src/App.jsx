dc2d0ba7f28120725f6da93ae89be5835b53c665
arimohy <arimohy@gmail.com>
2021-10-09 23:54:19 -0500

    practicando react

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React,{useState, useRef,useEffect} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import { TodoList } from './components/TodoList'
+
+const KEY='todoApp.todos';
+export function App(){
+    const[todos,setTodos] =useState([{id:1,task:'tarea 1',completed:false}])
+    const todoTaskRef=useRef();
+    const toggleTodo=(id)=>{
+        const newTodos=[...todos]
+        const todo=newTodos.find((todo)=>todo.id===id)
+        todo.completed=!todo.completed;
+        setTodos(newTodos)
+    }
+    const handleTodoAdd=()=>{
+        const task=todoTaskRef.current.value;
+        if(task==="")return;
+        setTodos((prevTodos)=>{
+            return[...prevTodos,{id:uuidv4() ,task,completed:false}]
+        });
+        todoTaskRef.current.value=null;
+    }
+    const handleClearAll=()=>{
+        const newTodos=todos.filter((todo)=>!todo.completed);
+        setTodos(newTodos)
+    }
+    useEffect(()=>{
+        const storedTodos=JSON.parse(localStorage.getItem(KEY))
+        if(storedTodos){
+            setTodos(storedTodos)
+        }
+    },[])
+    useEffect(()=>{
+        localStorage.setItem(KEY,JSON.stringify(todos))
+    },[todos])
+    return (
+        <>
+        <TodoList todos={todos}  toggleTodo={toggleTodo}/>
+        <input ref ={todoTaskRef}type="text" placeholder="Nueva Tarea" />
+        <button onClick={handleTodoAdd}>+</button>
+        <button onClick={handleClearAll}>🗑️</button>
+        <div> Te quedan {todos.filter((todo)=>!todo.completed).length} tareas por terminar</div>
+        </>
+    )
+}
